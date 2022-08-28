c7c7f9f4de03e6bb3667bc08382aaa143b40b0ae
ljs294 <ljsmall44@gmail.com>
2022-08-27 17:09:04 -0700

    Completes exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    const args = Array.prototype.slice.call(arguments,1);
 
-};
+    for (let i = 0; i < args.length; i++) {
+        if (array.includes(args[i])) {
+            //console.log('IF1')
+            array.splice(array.indexOf(args[i]),1);
+        }
+    }
 
-// Do not edit below this line
-module.exports = removeFromArray;
+   // console.log(array);
+    return array;
+}
+    // Do not edit below this line
+    module.exports = removeFromArray;
